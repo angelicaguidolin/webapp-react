41444d9788adfc3b8365eb1159f441d0513e0e17
angelica <guidolinangelica66@gmail.com>
2025-02-28 16:46:05 +0100

    feat add homepage and single page movie

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+//PAGES
+import HomePage from "./pages/Homepage";
+import SingleMovie from "./pages/SingleMovie";
 export default function App(){
-  
+  return(
+    <>
+    <BrowserRouter>
+    <Routes>
+        <Route index path="/" element={<HomePage/>} />
+        <Route path="/movies/:id" element={<SingleMovie/>} />
+    </Routes>
+</BrowserRouter>
+</>
+  )
 }
---
+++ src/pages/SingleMovie.jsx
@@ -0,0 +1,3 @@
+export default function SingleMovie(){
+    return (<h1>QUA VERRANNO INSERITI I SINGOLI FILM</h1>)
+}
